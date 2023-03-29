bb9a4d1a6f865dc2fe1035b72ae2e2e49c1505a3
yiptsunho <thyipac@connect.ust.hk>
2023-03-29 17:40:06 +0800

    WS WIP

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express(); // app is an instance of express
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        console.log(data)
+        socket.to(data.room).emit("receive_message", data);
+        // socket.broadcast.emit("receive_message", data)
+    });
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+    });
+});
+
+server.listen(8081, () => {
+    console.log("SERVER IS RUNNING!!");
+});
