875f29a365a44d6721adaa82d70a3e3bfdc34bdc
yiptsunho <thyipac@connect.ust.hk>
2023-03-31 16:56:34 +0800

    i dont know whats wrong

--- index.js
+++ index.js
@@ -15,18 +15,48 @@ const io = new Server(server, {
     },
 });
 
+io.use((socket, next) => {
+
+    const userId = socket.handshake.auth.userId;
+    const loginId = socket.handshake.auth.loginId;
+    if (!userId || !loginId) {
+        return next(new Error("invalid userId or loginId"));
+    }
+    socket.userId = userId;
+    socket.loginId = loginId;
+    next();
+});
+
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
+    const users = [];
+    for (let [id, socket] of io.of("/").sockets) {
+        users.push({
+            socketId: id,
+            socket: socket,
+            userId: socket.userId,
+            loginId: socket.loginId,
+        });
+    }
+    console.log(users)
 
     socket.on("send_message", (data) => {
         console.log(data)
         socket.to(data.room).emit("receive_message", data);
-        // socket.broadcast.emit("receive_message", data)
+        // save message in database
     });
 
     socket.on("join_room", (data) => {
-        console.log(data)
-        socket.join(data)
+        // data = {
+        //     roomId: "",
+        //     receiverId: ""
+        // }
+        console.log("someone joined room " + data.roomId)
+        console.log(users)
+        let userIndex = users.findIndex(user => user.userId == data.receiverId);
+        let targetSocket = users[userIndex].socket;
+        console.log("user " + data.receiverId + " is invited to join room " + data.roomId)
+        targetSocket.join(data.roomId)
     });
 });
 
